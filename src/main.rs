75bd9e8d880ebb1f04be538b87b8e0bdad22189e
André Generoso <114500148+AndreGenerosoRepo@users.noreply.github.com>
2023-04-04 16:04:39 +0100

    stupid commits
    another one

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 #[macro_use] extern crate rocket;
+use rocket::fs::TempFile;
+use uuid::Uuid;
+use std::path::Path;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[post("/upload", format = "plain", data = "<file>")]
+async fn upload(mut file: TempFile<'_>) -> std::io::Result<()> {
+    let id = Uuid::new_v4();
+    let form = format!("./files/{}", id.to_string());
+    let path = Path::new(&form);
+    file.persist_to(path).await
+}
+
+#[get("/download/<identifier>")]
+fn download(identifier: &str) -> &'static str {
+    "You did a heckin upload fam!"
+}
+
+#[delete("/delete/<identifier>")]
+fn delete(identifier: &str) -> &'static str {
+    "OMG i just deleted!"
+}
+
+#[put("/replace/<identifier>")]
+fn replace(identifier: &str) -> &'static str {
+    "I just replaced a file!"
+}
+
+#[get("/list")]
+fn list() -> &'static str {
+    "Here are all the files!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/v1", routes![upload,download,delete,replace])
 }
